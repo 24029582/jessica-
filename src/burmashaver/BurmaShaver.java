d3e771520330e843e293925e96c78bd47b11b17e
24029582 <fhulufhelojessicah@gmail.com>
2025-03-20 16:45:23 +0200

    Add files via upload

---
+++ src/burmashaver/BurmaShaver.java
@@ -0,0 +1,16 @@
+
+package burmashaver;
+
+public class BurmaShaver {
+public static void main(String[] args) {
+
+System.out.print(" EVERY SHAVER ");    
+System.out.print(" NOW CAN SNORE ");    
+System.out.print(" SIX MORE MINUTES ");    
+System.out.print(" THAN BEFORE ");    
+    
+    
+    
+    }
+    
+}
